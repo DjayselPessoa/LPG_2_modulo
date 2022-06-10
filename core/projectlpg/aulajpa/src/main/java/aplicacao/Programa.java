5fb93f9535b6b973af117f516d71ca891418af50
Djaysel Pessôa <djaysel@outlook.com>
2022-06-10 01:03:20 -0300

    up

--- core/projectlpg/aulajpa/src/main/java/aplicacao/Programa.java
+++ core/projectlpg/aulajpa/src/main/java/aplicacao/Programa.java
@@ -1,27 +1,12 @@
 package aplicacao;
 
-import java.util.Scanner;
-
-import dominio.Pessoa;
 import processos.DigitoUnico;
 
 public class Programa {
     
     public static void main(String[] args){
-        Pessoa p1 = new Pessoa(1, "Djaysel Pessoa", "djaysel@gmail.com");
-        Pessoa p2 = new Pessoa(2, "Ben", "ben@gmail.com");
-        Pessoa p3 = new Pessoa(3, "Lih", "lih@gmail.com");
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
-
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("Informe um nome: ");
-        String nome = String.valueOf(entrada.nextLine());
-        System.out.println("Informe um número inteiro: ");
-        Integer numberFim = Integer.parseInt(String.valueOf(entrada.nextInt()));
-        entrada.close();
-        DigitoUnico.digitounico(nome,numberFim);
+        
+        DigitoUnico.digitounico();
         
     }
 
--- core/projectlpg/aulajpa/src/main/java/processos/DigitoUnico.java
+++ core/projectlpg/aulajpa/src/main/java/processos/DigitoUnico.java
@@ -1,31 +1,67 @@
 package processos;
 
+import java.util.Scanner;
+
 public class DigitoUnico {
-    public static void digitounico(String name, Integer numberInt){
-        
-        String numberStr = numberInt.toString();
+    public static void digitounico(){
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Informe um nome: ");
+        String nome = String.valueOf(entrada.nextLine());
+
+        System.out.println("Informe um número inteiro: ");
+        Integer numberFim = Integer.parseInt(String.valueOf(entrada.nextInt()));
+
+        entrada.close();
+        String numberStr = numberFim.toString();
+
         char[] digitosLista = numberStr.toCharArray();
+        Integer ant = 0;
         Integer num = 0;
         Integer cont = 0;
-        // System.out.println("ok1");
+        
         for (int i =0; i < digitosLista.length; i++) {
-            num = num + Integer.parseInt(String.valueOf(digitosLista[i]));
-            System.out.println((cont+1)+"º valor de num: "+num);
+           
+            if(cont == 0){
+                num = num + Integer.parseInt(String.valueOf(digitosLista[i]));
+                ant = num;
+                System.out.println((cont+1)+"º valor de num: "+num);
+            }else{
+                num = num + Integer.parseInt(String.valueOf(digitosLista[i]));
+                System.out.println("-> "+ant+" + "+String.valueOf(digitosLista[i])+" ");
+                ant = num;
+                System.out.println((cont+1)+"º valor de num: "+num);
+            }
+            
             cont++;
         }
         
         String numberStr2 = num.toString();
+
         num = 0;
         cont = 0;
+        ant = 0;
         char[] digitosLista2 = numberStr2.toCharArray();
-        
-        for (int x = 0; x < digitosLista2.length;x++){
-            num = num + Integer.parseInt(String.valueOf(digitosLista2[x]));
-            System.out.println((cont+1)+"º valor de num: "+num);
-            cont++;
-        }
+        if(digitosLista2.length < 3){
+            for (int x = 0; x < digitosLista2.length; x++){
+                if(cont == 0){
+                    num = num + Integer.parseInt(String.valueOf(digitosLista2[x]));
+                    ant = num;
+                    System.out.println((cont+1)+"º valor de num: "+num);
+                }else{
+                    num = num + Integer.parseInt(String.valueOf(digitosLista2[x]));
+                    System.out.println("-> "+ant+" + "+String.valueOf(digitosLista[x])+" ");
+                    ant = num;
+                    System.out.println((cont+1)+"º valor de num: "+num);
+                }
+                
+                cont++;
+            }
 
-        System.out.println("O valor final do número é: "+num);
+        }
+        
+
+        System.out.println("Olá "+nome+"\nO valor final do número é: "+num);
         
         
 
